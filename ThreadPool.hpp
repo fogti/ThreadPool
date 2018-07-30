467d2f4799bb4004a71f08088631c6a68057bbeb
Jan Niklas Hasse <jhasse@bixense.com>
2018-07-30 19:05:22 +0200

    Move destructor into a new cpp file

--- ThreadPool.hpp
+++ ThreadPool.hpp
@@ -77,16 +77,4 @@ auto ThreadPool::enqueue(F&& f, Args&&... args)
     return res;
 }
 
-// the destructor joins all threads
-ThreadPool::~ThreadPool()
-{
-    {
-        std::unique_lock<std::mutex> lock(queue_mutex);
-        stop = true;
-    }
-    condition.notify_all();
-    for(std::thread &worker: workers)
-        worker.join();
-}
-
 #endif
