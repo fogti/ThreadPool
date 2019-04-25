34ef790626273a2a9869f435ff5033921f7b1058
Erik Zscheile <erik.zscheile@gmail.com>
2019-04-26 00:02:24 +0200

    move constructor to ThreadPool.cpp

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -1,13 +1,36 @@
 #include "ThreadPool.hpp"
 
+// the constructor just launches some amount of workers
+ThreadPool::ThreadPool(size_t threads)
+  : stop(false)
+{
+  for(size_t i = 0;i<threads;++i)
+    workers.emplace_back([this] {
+      for(;;) {
+        std::packaged_task<void()> task;
+
+        {
+          std::unique_lock<std::mutex> lock(this->queue_mutex);
+          this->condition.wait(lock,
+            [this]{ return this->stop || !this->tasks.empty(); });
+          if(this->stop && this->tasks.empty())
+            return;
+          task = std::move(this->tasks.front());
+          this->tasks.pop();
+        }
+
+        task();
+      }
+    });
+}
+
 // the destructor joins all threads
 ThreadPool::~ThreadPool() {
-	{
-		std::unique_lock<std::mutex> lock(queue_mutex);
-		stop = true;
-	}
-	condition.notify_all();
-	for (std::thread& worker : workers) {
-		worker.join();
-	}
+  {
+    std::unique_lock<std::mutex> lock(queue_mutex);
+    stop = true;
+  }
+  condition.notify_all();
+  for(std::thread& worker : workers)
+    worker.join();
 }
--- ThreadPool.hpp
+++ ThreadPool.hpp
@@ -23,34 +23,6 @@ private:
     bool stop;
 };
 
-// the constructor just launches some amount of workers
-inline ThreadPool::ThreadPool(size_t threads)
-    :   stop(false)
-{
-    for(size_t i = 0;i<threads;++i)
-        workers.emplace_back(
-            [this]
-            {
-                for(;;)
-                {
-                    std::packaged_task<void()> task;
-
-                    {
-                        std::unique_lock<std::mutex> lock(this->queue_mutex);
-                        this->condition.wait(lock,
-                            [this]{ return this->stop || !this->tasks.empty(); });
-                        if(this->stop && this->tasks.empty())
-                            return;
-                        task = std::move(this->tasks.front());
-                        this->tasks.pop();
-                    }
-
-                    task();
-                }
-            }
-        );
-}
-
 // add new work item to the pool
 template<class F, class... Args>
 decltype(auto) ThreadPool::enqueue(F&& f, Args&&... args)
